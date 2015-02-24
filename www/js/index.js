9b470bcde0e5238cccae243f818944d4492811c9
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-02-24 12:35:49 -0500

    Final project

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,13 @@
  * under the License.
  */
 var app = {
+    positionWatcher: 0,
+    headingWatcher: 0,
+
+    latitude: 0,
+    longitude: 0,
+    heading: 0,
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -27,6 +34,53 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('pause', this.onPause, false);
+        document.addEventListener('resume', this.onResume, false);
+
+        document.addEventListener('swipedown', app.onExit);
+    },
+
+    onExit: function() {
+        app.onPause();
+
+        console.log('Exiting application');
+        if (navigator.app) {
+            navigator.app.exitApp();
+        } else if (navigator.device) {
+            navigator.device.exitApp();
+        }
+    },
+
+    onPause: function() {
+        console.log('Pausing application');
+        if (app.positionWatcher) {
+            console.log('Canceling position watcher:', app.positionWatcher);
+            navigator.geolocation.clearWatch(app.positionWatcher);
+            app.positionWatcher = 0;
+        }
+
+        if (app.headingWatcher) {
+            console.log('Canceling heading watcher:', app.headingWatcher);
+            navigator.compass.clearWatch(app.headingWatcher);
+            app.headingWatcher = 0;
+        }
+    },
+
+    onResume: function() {
+        console.log('Resuming application');
+        app.onDeviceReady();
+    },
+
+    updateBackgroundColor: function() {
+        var canvas = document.getElementById('canvas');
+        if (!canvas) {
+            return;
+        }
+
+        canvas.style.backgroundColor = 'rgb(' +
+            Math.floor(Math.abs(app.latitude)) + ',' +
+            Math.floor(Math.abs(app.longitude)) + ',' +
+            (Math.floor(app.heading) % 255) + ')';
     },
 
     onError: function(error) {
@@ -38,12 +92,17 @@ var app = {
             longitude = position.coords.longitude;
 
         console.log('Latitude: ' + latitude + ' Longitude: ' + longitude);
+        app.latitude = latitude;
+        app.longitude = longitude;
+        app.updateBackgroundColor();
     },
 
     onWatchHeading: function(heading) {
         var magneticHeading = heading.magneticHeading;
 
         console.log('Magnetic heading: ' + magneticHeading);
+        app.heading = magneticHeading;
+        app.updateBackgroundColor();
     },
 
     // deviceready Event Handler
@@ -51,29 +110,16 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-
-        document.addEventListener('swipedown', function() {
-            if (navigator && navigator.app) {
-                navigator.app.exitApp();
-            } else {
-                console.log("Exiting Application");
-            }
-        });
+        document.addEventListener('swipedown', app.onExit);
 
         // Watch for changes in position
-        window.geolocation.watchPosition(app.onWatchPosition, app.onError);
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        if (!app.positionWatcher) {
+            app.positionWatcher = navigator.geolocation.watchPosition(app.onWatchPosition, app.onError);
+        }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        if (!app.headingWatcher) {
+            app.headingWatcher = navigator.compass.watchHeading(app.onWatchHeading, app.onError);
+        }
     }
 };
 
